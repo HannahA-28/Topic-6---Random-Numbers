11f8073fcd3db92f2026173b5c81418b26565385
Hannah Anderson <339729345@GoTVDSB.ca>
2025-03-24 14:31:07 -0400

    Add project files.

---
+++ Topic 6 - Random Numbers/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic_6___Random_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
