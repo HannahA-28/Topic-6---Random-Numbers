c40a30a947b36cfb64a327a9b55698c76cd2d8ac
Hannah Anderson <339729345@GoTVDSB.ca>
2025-03-24 14:40:24 -0400

    Just starting!

--- Topic 6 - Random Numbers/Program.cs
+++ Topic 6 - Random Numbers/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Random generator = new Random();
+            int randNum;
+            randNum = generator.Next(10);
+            Console.WriteLine("My random number is " + randNum);
+
+            Console.WriteLine("Here are some numbers from 0 to 4!");
+            Console.Write(generator.Next(5) + " ");
+            Console.Write(generator.Next(5) + " ");
+            Console.Write(generator.Next(5) + " ");
+            Console.Write(generator.Next(5) + " ");
+            Console.Write(generator.Next(5) + " ");
+            Console.WriteLine(generator.Next(5) + " ");
+            Console.WriteLine();
+
+            Console.WriteLine("Here are some numbers from 0 to 99!");
+            Console.Write(generator.Next(100) + " ");
+            Console.Write(generator.Next(100) + " ");
+            Console.Write(generator.Next(100) + " ");
+            Console.Write(generator.Next(100) + " ");
+            Console.Write(generator.Next(100) + " ");
+            Console.WriteLine(generator.Next(100) + " ");
+            Console.WriteLine();
         }
     }
 }
